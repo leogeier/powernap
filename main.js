2efb9a2b605b67bf58d64c3ca039e9e49959adda
leogeier <leonard.geier@hotmail.com>
2020-04-14 17:31:57 +0200

    Nice face

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-const alarm = new Audio("alarm.mp3");
+let alarm, animationElements;
+window.onload = () => {
+  alarm = new Audio("alarm.mp3");
+  animationElements = [ document.getElementsByTagName("body")[0], document.getElementsByClassName("title")[0] ];
+}
 
-function handleNapClick() {
+function handleNapDown() {
+  for (const e of animationElements) {
+    e.classList.remove("fade-dark-light");
+    e.classList.add("fade-light-dark");
+  }
+}
+
+function handleNapUp() {
   alarm.play();
+
+  for (const e of animationElements) {
+    e.classList.remove("fade-light-dark");
+    e.classList.add("fade-dark-light");
+  }
 }
